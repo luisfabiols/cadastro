fd3b09dba918502317e7a1d425e94ec7991b5400
luisfabiols <43914634+luisfabiols@users.noreply.github.com>
2026-08-17 00:36:09 -0300

    feat: atualizaçao do processacadastro

--- processaCadastro.php
+++ processaCadastro.php
@@ -1,2 +1,8 @@
 <?php
+<?php
+echo "Nome: ".$_POST['nome']."<br>";
+echo "Idade: ".$_POST['idade']."<br>";
+echo "Profissão: ".$_POST['profi']."<br>";
+echo "Salario: ".$_POST['sal']."<br>";
+echo "Experiencia anterior: ".$_POST['exp']."<br>";
 ?>
